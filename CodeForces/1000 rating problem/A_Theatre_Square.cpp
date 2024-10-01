8dc8b11245f4b50f2b92021987eb19ddf7bfbda2
anisul alam <anisulalam2002@gmail.com>
2024-10-01 12:27:24 +0600

    1-10-24

---
+++ CodeForces/1000 rating problem/A_Theatre_Square.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n,m,a;
+    cin>>n>>m>>a;
+    long long ans = ((n + a - 1) / a) * ((m + a - 1) / a);
+    cout<<ans<<endl;
+    return 0;
+}
